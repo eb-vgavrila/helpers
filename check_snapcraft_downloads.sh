b4fcd765c2e2f2f4d0b516708a711688a00cf97b
eb-vgavrila <126121198+eb-vgavrila@users.noreply.github.com>
2025-12-19 08:38:37 -0800

    Update check_snapcraft_downloads.sh
    XML has been minified

--- check_snapcraft_downloads.sh
+++ check_snapcraft_downloads.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
-
 curl -s https://status.snapcraft.io/rss > snap_status.xml
 
 RSS_DISABLED=$(cat snap_status.xml | grep 'RSS feed disabled for this page')
 if [[ $RSS_DISABLED == *'RSS feed disabled'* ]]; then echo "RSS status is unavailable. Winging it!"; exit 0; fi
 
-SNAP_STATUS=$(cat snap_status.xml | grep 'Snap downloads is' | cut -d '>' -f2 | cut -d '<' -f1)
-PUB_DATE=$(cat snap_status.xml | grep -m 1 'pubDate' | cut -d '>' -f2 | cut -d '<' -f1)
+# Extract "Snap downloads" status from the minified XML using sed to extract the description
+SNAP_STATUS=$(cat snap_status.xml | sed -n 's/.*<description>Snap downloads is \([^<]*\)<\/description>.*/\1/p')
+PUB_DATE=$(cat snap_status.xml | sed -n 's/.*<pubDate>\([^<]*\)<\/pubDate>.*/\1/p' | head -1)
 
-if [[ $SNAP_STATUS != *"Operational"* ]]; then echo "$SNAP_STATUS at $PUB_DATE. Halting the pipeline!"; exit 99;
+if [[ $SNAP_STATUS != *"Operational"* ]]; then echo "$SNAP_STATUS at $PUB_DATE. Halting the pipeline!"; exit 18;
 else echo "$SNAP_STATUS at $PUB_DATE"; fi
